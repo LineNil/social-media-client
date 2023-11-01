c5fd5e05bd0b3218d8f438a6446dc23dde13274f
Line Nilsen <lineforslund@hotmail.com>
2023-11-01 10:35:41 +0100

    login test update

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,46 @@
+import { login } from './login';
+import localStorageMock from './localStorageMock';
+
+jest.mock('../constants', () => ({ apiPath: 'mocked-api-path' }));
+jest.mock('../headers', () => ({ headers: jest.fn() }));
+
+describe('login function', () => {
+  beforeAll(() => {
+    global.localStorage = localStorageMock;
+  });
+
+  afterAll(() => {
+    global.localStorage = null;
+  });
+
+  it('should fetch and store a token in browser storage on success', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue({ accessToken: 'mocked-token' }),
+    });
+
+    localStorageMock.save.mockImplementation(() => {});
+
+    const email = 'test@example.com';
+    const password = 'password123';
+
+    const result = await login(email, password);
+
+    expect(result).toEqual({ accessToken: 'mocked-token' });
+    expect(localStorageMock.save).toHaveBeenCalledWith('token', 'mocked-token');
+    expect(localStorageMock.save).toHaveBeenCalledWith('profile', {
+      accessToken: 'mocked-token',
+    });
+  });
+
+  it('should throw an error on API request failure', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      statusText: 'Unauthorized',
+    });
+
+    await expect(login('test@example.com', 'password123')).rejects.toThrow(
+      'Unauthorized',
+    );
+  });
+});
