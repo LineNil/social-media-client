20aa2d6cce8e5136c750da8b5ef900161b319dd9
Line Nilsen <lineforslund@hotmail.com>
2023-11-06 10:38:10 +0100

    login e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,26 +4,20 @@ import { save } from '../../src/js/storage/index.js';
 
 describe('User Login and Profile Access', () => {
   it('should allow the user to log in and access their profile', () => {
-    cy.visit('index.html'); // Besøk forsiden
+    cy.visit('index.html');
 
-    // Finn og klikk på "Logg inn"-knappen øverst
     cy.contains('Login').click();
 
-    // Fyll innloggingsdetaljer
     cy.get('#loginEmailInput').type('geir@noroff.no');
     cy.get('#loginPassword').type('gggggggg');
 
-    // Klikk på logg inn-knappen
     cy.get('#logInButton').click();
 
-    // Verifiser at innloggingen var vellykket ved å sjekke URL
-    //cy.url().should('include', 'index.html');
     cy.url().should(
       'include',
-      'http://localhost:60447/?view=profile&name=geir',
+      'http://localhost:61253/?view=profile&name=geir',
     );
 
-    // Verifiser at brukerens profilside vises, for eksempel ved å sjekke teksten
     cy.contains('geir').should('be.visible');
   });
 });
