767680ab6a889dd76b2a3fce33907f519afea2e7
Line Nilsen <lineforslund@hotmail.com>
2023-11-10 10:48:47 +0100

    invalidLogin edit

--- cypress/e2e/invalidLogin.cy.js
+++ cypress/e2e/invalidLogin.cy.js
@@ -1,8 +1,12 @@
+/* eslint-disable jest/valid-expect */
+
 describe('Invalid Login Credentials', () => {
   it('should show an error message for invalid credentials', () => {
     cy.visit('index.html');
 
     cy.contains('Login').click();
+
+    cy.wait(1000);
 
     cy.get('#loginEmailInput').type('geir@noroff.no');
     cy.get('#loginPassword').type('invalidpassword');
@@ -10,8 +14,7 @@ describe('Invalid Login Credentials', () => {
     cy.get('#logInButton').click();
 
     cy.on('window:alert', (text) => {
-      cy.wrap(text).should(
-        'eq',
+      expect(text).to.equal(
         'Either your username was not found or your password is incorrect',
       );
     });
